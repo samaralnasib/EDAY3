d3a81b1d051877309304e544360207edd3c689d4
samaralnasib <148685912+samaralnasib@users.noreply.github.com>
2023-10-25 01:32:54 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+import java.util.Random;
+
+
+    public class Main {
+        public static void main(String[] args) {
+
+            //Q1
+            Scanner input = new Scanner(System.in);
+            //System.out.println("print your role");
+            //String role = input.next();
+
+            //if (role.equals("admin")) {
+            //    System.out.println("welcome admin");
+            //} else if (role.equals(" superuser")) {
+            //    System.out.println("welcome superuser");
+
+            //} else System.out.println("welcome user");
+            //}
+
+            //Q2
+            //System.out.println("enter num1");
+            //int num1 = (input.nextInt());
+
+            //System.out.println("enter num2");
+            //int num2 = (input.nextInt());
+
+            //System.out.println("enter num3");
+            //int num3 = (input.nextInt());
+
+            //int greatest = num1;
+            //if (num2 > greatest)
+            //{
+            //   greatest = num2;
+            // }
+            //else if (num3 > greatest) {
+            //    greatest = num3;
+            // }
+            //System.out.println("The greatest num is: " + greatest);
+
+            //Q5
+            //System.out.println("your age?");
+            //int age = input.nextInt();
+            // if (age<13) {
+            //    System.out.println("you are a child");
+            // } else if (age >=13 && age <=19) {
+            //    System.out.println("you are a teenager");
+            //} else {
+            //   System.out.println("you are an adult");
+        }
+        //Q3
+        //Random random = new Random();
+        //int x = random.nextInt(7)+1;
+        //switch (x){
+            //case 1 : System.out.println("Sunday");
+                //break;
+            //case 2 : System.out.println("Monday");
+                //break;
+            //case 3 : System.out.println("Tuesday");
+                //break;
+            //case 4 : System.out.println("Wednesday");
+                //break;
+            //case 5 : System.out.println("Thursday");
+                //break;
+            //case 6 : System.out.println("Friday");
+                //break;
+            //case 7 : System.out.println("Saturday");
+                //break;
+            //default: System.out.println("end");
+        }
+        //Q4
+        //System.out.print("Enter the numeric score");
+                //int numericscore = input.nextInt();
+                //String grade;
+
+                //if (numericscore >= 90 && numericscore <= 100) {
+                //grade = "A";
+                //} else if (numericscore >= 80 && numericscore <= 89)
+                //{
+                //grade = "B";
+                //} else if (numericscore >= 70 && numericscore <= 79)
+                //{
+                //grade = "C";
+                //} else if (numericscore >= 60 && numericscore <= 69)
+                //{
+                //grade = "D";
+                //} else {
+                //grade = "F";
+                //}
+
+                //System.out.println("The corresponding letter grade is : " + grade);}
+
+
+
+
+
+
